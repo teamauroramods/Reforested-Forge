b56fc4709bae55b02a6bf4bef067c6b196fd69f6
Epic312 <kaden_ostendorf@yahoo.com>
2020-08-29 13:34:31 -0600

    birch saplings have a 10% chance to be tall

--- src/main/java/com/teamaurora/reforested/core/other/ReforestedEvents.java
+++ src/main/java/com/teamaurora/reforested/core/other/ReforestedEvents.java
@@ -28,7 +28,11 @@ public class ReforestedEvents {
                 event.setResult(Event.Result.DENY);
                 world.setBlockState(pos, Blocks.AIR.getDefaultState());
                 ConfiguredFeature<BaseTreeFeatureConfig, ?> configuredFeature;
-                configuredFeature = ReforestedFeatures.TALL_BIRCH_TREE.withConfiguration(ReforestedBiomeFeatures.BIRCH_TREE_CONFIG);
+                if (rand.nextInt(10) == 0) {
+                    configuredFeature = ReforestedFeatures.TALL_BIRCH_TREE.withConfiguration(ReforestedBiomeFeatures.BIRCH_TREE_CONFIG);
+                } else {
+                    configuredFeature = ReforestedFeatures.BIRCH_TREE.withConfiguration(ReforestedBiomeFeatures.BIRCH_TREE_CONFIG);
+                }
                 if (!configuredFeature.func_236265_a_(world, world.func_241112_a_(), world.getChunkProvider().getChunkGenerator(), rand, pos)) {
                     world.setBlockState(pos, state);
                 }
